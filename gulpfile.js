18172e1360814c80411c1ebc01bf5045dc5860c5
Alexcrudu <60149530+Alexcrudu@users.noreply.github.com>
2020-09-10 23:01:30 +0200

    Автоматизируем сборку личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,28 @@ const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const rename = require("gulp-rename");
+const del = require("del");
+const csso = require('gulp-csso');
+const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
 const sync = require("browser-sync").create();
 
 // Styles
 
 const styles = () => {
   return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(sync.stream());
-}
+  .pipe(plumber())
+  .pipe(sourcemap.init())
+  .pipe(less())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(csso())
+  .pipe(sourcemap.write("."))
+  .pipe( gulp.dest('build/css') )
+  .pipe(sync.stream());
+ }
 
 exports.styles = styles;
 
@@ -39,6 +45,20 @@ const server = (done) => {
 
 exports.server = server;
 
+// Server
+
+const buildServer = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
@@ -46,6 +66,55 @@ const watcher = () => {
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
+
+// Image
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.svgo()
+  ]))
+}
+
+exports.images = images;
+
+//Webp
+
+const converToWebP = ( ) => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 75}))
+  .pipe(gulp.dest("source/img"))
+}
+
+exports.webp = converToWebP;
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
+
+const copy = () => {
+  return gulp.src([
+  "source/fonts/**/*.{woff,woff2}",
+  "source/img/**",
+  "source/js/**",
+  "source/*.ico",
+  "source/*.html"
+  ], {
+    base: "source"
+  })
+ .pipe(gulp.dest('build'))
+ }
+
+ exports.copy = copy;
+
+exports.build = gulp.series(
+  clean, copy, styles, images, converToWebP, buildServer
+);
+
 exports.default = gulp.series(
   styles, server, watcher
-);
+)
